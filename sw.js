f15f04440aa10dc97703b9fc28ebd2448ae524dd
Archer Leida <archer@Archers-Laptop.local>
2026-02-27 09:57:24 -0800

    Redesign staffing: full-width row, hourly rate x hours per staff member

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ticket-calc-v2';
+const CACHE = 'ticket-calc-v3';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
