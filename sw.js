a5b4516b38e5889364f61822bb3addf95c479b69
Archer Leida <archer@Archers-Laptop.local>
2026-02-27 09:52:14 -0800

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ticket-calc-v1';
+const CACHE = 'ticket-calc-v2';
 const ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
